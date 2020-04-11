dd4d77c333e5c14e40b331105093e173d36d3b29
Ronni <aomab77@gmail.com>
2020-04-11 15:38:10 +0200

    FS main panel UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppComponent} from './app.component';
 import {LeftPanelComponent} from './left-panel/left-panel.component';
 import {ChannelService} from '../service/channel.service';
 import {HttpClientModule} from '@angular/common/http';
+import { MainPanelComponent } from './main-panel/main-panel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftPanelComponent
+    LeftPanelComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule,
